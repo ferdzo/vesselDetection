d9ecbf413b42cf4c10693e03f40b1d8482765aac
mveco <marija.vecovska@halicea.com>
2025-06-26 18:14:18 +0200

    Updated version with yolo v11

---
+++ train.py
@@ -0,0 +1,26 @@
+import os
+from ultralytics import YOLO
+
+model = YOLO("yolo11x.pt")
+
+data_path = 'ships-aerial-images/data.yaml'
+
+train_params = {
+    'epochs': 20,               
+    'batch': 8,
+    'imgsz': 640,
+    'lr0': 0.01,
+    'lrf': 0.01,
+    'momentum': 0.937,
+    'weight_decay': 0.0005,
+    'optimizer': 'AdamW',
+    'device': '0,1',
+    'pretrained': True,
+    'project': 'runs/train',
+    'name': 'vessel_training',
+    'exist_ok': True,
+    'save_period': 2,
+    'workers': 8,
+}
+
+model.train(data=data_path, **train_params)
