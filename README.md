e698a54eb16f90554988e0605f563e12a61a1054
ferdzo <andrej@ferdzo.xyz>
2025-06-29 00:42:33 +0200

    Updates

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 ### Ship detection using Machine Learning methods for the course Digital Processing of Image(Дигитално Процесирање на Слика)
 
 This is a simple ship detection model, made using pre-trained YOLO with COCO weights.
-Using super-gradients as a framework for PyTorch, for ease of use and faster prototyping.
--- train.py
+++ train.py
@@ -1,27 +1,28 @@
-import os
 from ultralytics import YOLO
 
-model = YOLO("yolo11x.pt")
+model = YOLO("yolo11l.pt")
 
 data_path = 'ships-aerial-images/data.yaml'
 
 train_params = {
-    'epochs': 20,               
-    'batch': 8,
+    'epochs': 40,
+    'batch': 32,
     'imgsz': 640,
-    'lr0': 0.01,
-    'lrf': 0.01,
+    'lr0': 5e-4,
+    'lrf': 0.1,
+    'warmup_epochs': 5,
+    'warmup_bias_lr': 1e-6,
     'momentum': 0.937,
-    'weight_decay': 0.0005,
+    'weight_decay': 0.0001,
     'optimizer': 'AdamW',
     'device': '0,1',
-    'pretrained': True,
     'project': 'runs/train',
-    'name': 'vessel_training',
+    'name': 'vessel_deteciton_v11l',
     'exist_ok': True,
     'save_period': 2,
     'workers': 8,
-    'auto_augment': 'autoaugment',
+    'patience': 20,           
+    'cos_lr': True,            
 }
 
 model.train(data=data_path, **train_params)
